1aa8ebc8adbc7750ae65fb1ddd7251169fa70cdb
Gonçalo Fonseca (1150503) <1150503@isep.ipp.pt>
2018-12-08 14:35:48 +0000

    Fix for gitIgnore

---
+++ src/main/java/main.java
@@ -0,0 +1,5 @@
+/**
+ * @author Gonçalo Fonseca (1150503@isep.ipp.pt)
+ */
+public class main {
+}
---
+++ src/test/java/mainTest.java
@@ -0,0 +1,9 @@
+/**
+ * @author Gonçalo Fonseca (1150503@isep.ipp.pt)
+ */
+public class mainTest {
+
+    public static void main(String[] args) {
+        System.out.println("Ok");
+    }
+}
